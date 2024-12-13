c13ba30f24f2770ea3026563fd2ff667f34a967f
Liam <lkilkenny97@gmail.com>
2024-12-13 17:19:56 +0000

    fix: resolve test configuration

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,11 @@
 module.exports = {
   testEnvironment: 'jsdom',
   moduleNameMapper: {
-    '^@/(.*)$': './src/$1',
+    '^@/(.*)$': '<rootDir>/src/$1'
   },
+  testMatch: [
+    "<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}",
+    "<rootDir>/src/**/*.{spec,test}.{js,jsx,ts,tsx}"
+  ],
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
 };
